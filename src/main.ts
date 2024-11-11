7dd55dcfc040b516bf9700cb1eb28aeaa5f9442f
Gustaf Hultgren <gustaf.hultgren@gmail.com>
2024-11-11 20:52:41 +0100

    feat: add lazors

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,45 @@
 import './style.css'
 
+const root = document.documentElement;
+
+const SHIP_HEIGHT = getComputedStyle(root).getPropertyValue('--ship-height');
+const SHIP_WIDTH = getComputedStyle(root).getPropertyValue('--ship-width');
+const SHIP_OFFSET_Y = getComputedStyle(root).getPropertyValue('--ship-offset-y');
+
 const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
   <div class="spacecraft"></div>
 `
 
-let currentButtonPressed: 'ArrowLeft' | 'ArrowRight' | null = null
+let currentArrowPressed: 'ArrowLeft' | 'ArrowRight' | null = null
+let isFiring = false
+
 window.addEventListener('keydown', (event) => {
   if (event.key === 'ArrowLeft') {
-    currentButtonPressed = 'ArrowLeft'
+    currentArrowPressed = 'ArrowLeft'
   }
 
   if (event.key === 'ArrowRight') {
-    currentButtonPressed = 'ArrowRight'
+    currentArrowPressed = 'ArrowRight'
+  }
+
+  if (event.key === ' ') {
+    isFiring = true
   }
 })
 
 window.addEventListener('keyup', (event) => {
-  if (event.key === 'ArrowLeft' && currentButtonPressed === 'ArrowLeft') {
-    currentButtonPressed = null
+  if (event.key === 'ArrowLeft' && currentArrowPressed === 'ArrowLeft') {
+    currentArrowPressed = null
   }
 
-  if (event.key === 'ArrowRight' && currentButtonPressed === 'ArrowRight') {
-    currentButtonPressed = null
+  if (event.key === 'ArrowRight' && currentArrowPressed === 'ArrowRight') {
+    currentArrowPressed = null
+  }
+
+  if (event.key === ' ') {
+    isFiring = false
   }
 })
 
@@ -35,43 +51,93 @@ const BASE_ACCELERATION = 0.1
 const RETURN_ACCELERATION = 0.000001
 const MIDDLE_POS = MAX_POS / 2
 
-let pos = 50
-let speed = 0
-let acceleration = 0
+let shipPos = 50
+let shipSpeed = 0
+let shipAcceleration = 0
+
+const lasers: { element: HTMLDivElement, posY: number, posX: number }[] = []
+
+let lastFire = Date.now()
+let LASER_COOLDOWN = 150
+function shootLaser() {
+  const now = Date.now()
+
+  if (now - lastFire > LASER_COOLDOWN) {
+    const element = document.createElement('div')
+    element.classList.add('laser')
+
+    const posX = shipPos
+    const posY = 0
+
+    const laser = {
+      element,
+      posX,
+      posY
+    }
+
+    lasers.push(laser)
+    app.appendChild(element)
+
+    lastFire = now
+  }
+}
+
+const LASER_SPEED = 1
+
+function drawLasers() {
+  for (const laser of lasers) {
+    laser.posY += LASER_SPEED
+
+    if (laser.posY >= 100) {
+      laser.element.remove()
+      lasers.splice(lasers.indexOf(laser), 1)
+    }
+
+    laser.element.style.bottom = `calc(${laser.posY}vh + ${SHIP_OFFSET_Y} + ${SHIP_HEIGHT})`
+    laser.element.style.left = `calc(${laser.posX}vw + ${SHIP_WIDTH} / 2 - 1px)`
+  }
+}
 
 setInterval(() => {
-  if (currentButtonPressed === 'ArrowLeft') {
-    acceleration = -BASE_ACCELERATION
-  } else if (currentButtonPressed === 'ArrowRight') {
-    acceleration = BASE_ACCELERATION
+  if (isFiring) {
+    shootLaser()
+  }
+
+  drawLasers()
+
+
+  if (currentArrowPressed === 'ArrowLeft') {
+    shipAcceleration = -BASE_ACCELERATION
+  } else if (currentArrowPressed === 'ArrowRight') {
+    shipAcceleration = BASE_ACCELERATION
   } else {
-    const distanceToMiddle = pos - MIDDLE_POS
-    acceleration = -distanceToMiddle * RETURN_ACCELERATION
+    const distanceToMiddle = shipPos - MIDDLE_POS
+    shipAcceleration = -distanceToMiddle * RETURN_ACCELERATION
   }
 
-  if (pos <= 0) {
-    pos = 0
-    if (Math.abs(speed) >= BOUNCE_THRESHOLD) {
-      acceleration = 0
-      speed = -speed * DAMPING_FACTOR
+  if (shipPos <= 0) {
+    shipPos = 0
+    if (Math.abs(shipSpeed) >= BOUNCE_THRESHOLD) {
+      shipAcceleration = 0
+      shipSpeed = -shipSpeed * DAMPING_FACTOR
     } else {
-      speed = 0
+      shipSpeed = 0
     }
-  } else if (pos >= MAX_POS) {
-    pos = MAX_POS
-    if (Math.abs(speed) >= BOUNCE_THRESHOLD) {
-      acceleration = 0
-      speed = -speed * DAMPING_FACTOR
+  } else if (shipPos >= MAX_POS) {
+    shipPos = MAX_POS
+    if (Math.abs(shipSpeed) >= BOUNCE_THRESHOLD) {
+      shipAcceleration = 0
+      shipSpeed = -shipSpeed * DAMPING_FACTOR
     } else {
-      speed = 0
+      shipSpeed = 0
     }
   }
 
-  speed = Math.max(-MAX_SPEED, Math.min(MAX_SPEED, speed + acceleration))
+  shipSpeed = Math.max(-MAX_SPEED, Math.min(MAX_SPEED, shipSpeed + shipAcceleration))
 
-  pos = Math.max(0, Math.min(MAX_POS, pos + speed))
+  shipPos = Math.max(0, Math.min(MAX_POS, shipPos + shipSpeed))
 
   const spacecraft = document.querySelector<HTMLDivElement>('.spacecraft')!
 
-  spacecraft.style.left = `calc(max(0px, min(${pos}vw, 100vw - 24px))`
+  spacecraft.style.left = `calc(max(0px, min(${shipPos}vw, 100vw - 24px))`
 }, 1000 / 60 /* 60 FPS */)
